2f58bbf504707e5906d9ac05fcb9509ebea4d50e
buihuongquynh <buigiangquynh@gmail.com>
2022-04-27 13:41:44 +0700

    fix

---
+++ app/Http/Controllers/AuController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class AuController extends Controller
+{
+    public function getIndex()
+  {
+    return view('login', ['title' => 'login']);
+  }
+}
