0f65fb5b9c0e335375f59582bf6197bc56cdc34c
buihuongquynh <62638846+buihuongquynh@users.noreply.github.com>
2022-06-03 14:25:18 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -21,16 +21,21 @@ use App\Http\Controllers\OnLeaveController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('on-leave', OnLeaveController::class);
-Route::get('slow-of-user/{id}', [OnLeaveController::class, 'listTimeSlow']);
-Route::get('get-all-leave', [OnLeaveController::class, 'getAll']);
-Route::post('change-status/{id}', [OnLeaveController::class, 'changeStatus']);
-Route::post('timestamp', [TimestampController::class, 'create']);
-Route::get('timestamp', [TimestampController::class, 'getListTimestamp']);
-Route::get('timestamp/{id}', [TimestampController::class, 'show']);
-Route::get('time-of-user', [TimestampController::class, 'getTimeOfUser']);
-Route::post('timestamp/update/{id}', [TimestampController::class, 'update']);
-Route::post('create-edit-time', [TimestampController::class, 'createEditTime']);
-Route::post('createTimeOfDayNoCheckIn', [TimestampController::class, 'createTimeOfDayNoCheckIn']);
-Route::post('signUp', [Guard::class, 'signUp']);
-Route::post('image-upload', [Guard::class, 'upload']);
+
+
+// Route::middleware(['validate_token'])->group(function () {
+   
+//
+    Route::resource('on-leave', OnLeaveController::class);
+    Route::get('slow-of-user/{id}', [OnLeaveController::class, 'listTimeSlow']);
+    Route::get('get-all-leave', [OnLeaveController::class, 'getAll']);
+    Route::post('change-status/{id}', [OnLeaveController::class, 'changeStatus']);
+    Route::post('timestamp', [TimestampController::class, 'create']);
+    Route::get('timestamp', [TimestampController::class, 'getListTimestamp']);
+    Route::get('timestamp/{id}', [TimestampController::class, 'show']);
+    Route::get('time-of-user', [TimestampController::class, 'getTimeOfUser']);
+    Route::post('timestamp/update/{id}', [TimestampController::class, 'update']);
+    Route::post('create-edit-time', [TimestampController::class, 'createEditTime']);
+    Route::post('createTimeOfDayNoCheckIn', [TimestampController::class, 'createTimeOfDayNoCheckIn']);
+    Route::post('signUp', [Guard::class, 'signUp']);
+    Route::post('image-upload', [Guard::class, 'upload']);
